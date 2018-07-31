f5b0dc276b3443274588810dbca5770ee3ca3218
JairEscamilla <hernandez_yo14@outlook.es>
2018-07-31 12:03:36 -0500

    Funcion sprintf

---
+++ cap4/sprint.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+  char nom[] = "Pablo";
+  int edad = 39;
+  double altura = 1.70;
+  char salida[50];
+  sprintf(salida, "Mi nombre es %s, tengo %d y tengo %lf", nom, edad, altura);
+  printf("%s\n", salida);
+  return 0;
+}
