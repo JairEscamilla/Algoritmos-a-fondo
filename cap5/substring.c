d23ea984a2327ef73c0f3fab0923981b1d2b7522
JairEscamilla <hernandez_yo14@outlook.es>
2018-07-31 22:45:26 -0500

    Cuerpo de la funcion

--- cap5/substring.c
+++ cap5/substring.c
@@ -1,3 +1,15 @@
 #include <stdlib.h>
 // PROTOTIPO DE LA FUNCION
 char * subString(char*, int, int);
+// Cuerpo de la funcion
+char* substring(char* s, int desde, int hasta){
+  int n = hasta-desde;
+  char* ret = (char*) malloc(n+1);
+  int j = 0;
+  for(int i = desde; i < hasta; i++){
+    ret[j] = s[i];
+    j += 1;
+  }
+  ret[j] = '\0';
+  return ret;
+}
