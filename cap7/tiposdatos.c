c09acc9c5b7e5c16e32a33fb31cf9bfb2ce9c839
JairEscamilla <hernandez_yo14@outlook.es>
2018-08-07 21:10:40 -0500

    Tipos de datos definidos por el programador

---
+++ cap7/tiposdatos.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+// defino un nuevo tipo de datos
+typedef int Entero;
+int main(){
+  // Declaro una variable de tipo entero
+  Entero e = 5;
+  printf("%d\n", e);
+
+  return 0;
+}
