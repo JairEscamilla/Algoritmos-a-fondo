ac9e98dd4a831f09a339f790f8ea00d3289d494d
JairEscamilla <hernandez_yo14@outlook.es>
2018-08-11 01:52:50 -0500

    Tamaño de un archivo

---
+++ cap8/tamaño.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+// PROTOTIPO DE LA FUNCION
+long fileSize(FILE*);
+// DESARROLLO DE LA FUNCION
+long fileSize(FILE* f){
+  long actual = ftell(f);
+  fseek(f, 0, SEEK_END);
+  long ultimo = ftell(f);
+  fseek(f, actual, SEEK_SET);
+  return ultimo;
+}
+
+// FUNCION PRINCIPAL
+int main(){
+  FILE* arch;
+  char c;
+
+  // Abro el archivo
+  arch = fopen("DEMO.dat", "r+a");
+  long size = fileSize(arch);
+  printf("El archivo tiene %ld bytes", size);
+  return 0;
+}
