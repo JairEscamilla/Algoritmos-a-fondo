b7f60adaa46c9c2b648aba1640140a658d541ad8
JairEscamilla <hernandez_yo14@outlook.es>
2018-08-11 21:56:05 -0500

    Añadiendo al final

--- cap8/anade.c
+++ cap8/anade.c
@@ -1,26 +1,17 @@
 #include <stdio.h>
 #include <string.h>
 #include "alumnos.h"
-
-// PROTOTIPO DE LA FUNCION
 Alumno ingresoDatosXConsola();
-
-// FUNCION PRINCIPAL
 int main(){
   FILE* arch = fopen("ALUMNOS.dat", "r+b");
-  int n;
-  printf("Ingrese un numero de registro: ");
-  fflush(stdout);
-  scanf("%d", &n);
-  // Ingreso los nuevos datos por consola
+
   Alumno reg = ingresoDatosXConsola();
 
-  // Posiciono el identificador de posicion
-  fseek(arch, n*sizeof(Aluimno), SEEK_SET);
-
-  // Grabo el registro pisando los valores anteriores
+  fseek(arch, 0, SEEK_END);
   fwrite(&reg, sizeof(Alumno), 1, arch);
+
   fclose(arch);
+
   return 0;
 }
 
