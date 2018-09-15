2382d08e27e1b869805619487d92e310c34e462f
Jair Escamilla <hernandez_yo14@outlook.es>
2018-09-14 23:19:07 -0500

    Trabajando de noche 7u7

---
+++ cap8/fscan.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+  FILE* arch = fopen("texto.txt", "r+");
+  char nombre[11];
+  float altura;
+  int edad;
+  fscanf(arch, "%s %f %d", nombre, &altura, &edad);
+  while (!feof(arch)) {
+    printf("%s, %.2f, %d\n", nombre, altura, edad);
+    fscanf(arch, "%s %f %d", nombre, &altura, &edad);
+  }
+  return 0;
+}
