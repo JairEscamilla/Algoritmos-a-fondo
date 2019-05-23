6c964b02bd4ba2e780b2f6002f655ffbb9776769
Jair Escamilla <hernandez_yo14@outlook.es>
2019-05-23 00:55:47 -0500

    Argumentos de programa por consola

---
+++ Java/argumentos.java
@@ -0,0 +1,10 @@
+package libro.cap13;
+
+public class argumentos{
+    public static void main(String[] args){
+        for(int i = 0; i < args.length; i++){
+            System.out.println(args[i]);
+        }
+        System.out.println("Total: "+ args.length + " argumentos");
+    }
+}
