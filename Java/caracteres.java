addc1c47be4c15fbdc513f441c11d5b08c6abc9e
Jair Escamilla <hernandez_yo14@outlook.es>
2019-05-23 00:49:32 -0500

    Caracteres en java

---
+++ Java/caracteres.java
@@ -0,0 +1,9 @@
+package libro.cap13;
+
+public class caracteres{
+    public static void main(String[] args){
+        for(int i = 0; i < 5; i++){
+            System.out.println(i + "A");
+        }
+    }
+}
---
+++ Java/caracteres3.java
@@ -0,0 +1,10 @@
+package libro.cap13;
+public class caracteres3{
+    public static void main(String[] args){
+        char c;
+        for(int i = 'A'; i <= 'Z'; i++){
+            c = (char)i;
+            System.out.println(c);
+        }
+    }
+}
